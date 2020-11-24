d6ca370f01bff22df11ade5fbd274148faec2418
Kyle Vondra <k.vondra95@gmail.com>
2020-11-23 21:26:28 -0800

    added basic bug class constructor
    This isn't the final constructor, just a filler for class. Will go back and add proper constructor and enums.

--- Bug_Tracker/Bug.h
+++ Bug_Tracker/Bug.h
@@ -9,12 +9,20 @@ class Bug
 {
 private:
 	int ID; // randomly generated
-	string Title;
+	string title;
 	int priority; // enum (P0, P1, P2)
 	int status; // enum (new, in-progress, completed)
 	string assignee; // email
 
-private:
+public:
+	Bug() {}; // default constructor
+	Bug(int ID_c, string title_c, int priority_c, int status_c, string assignee_c) : 
+		ID(ID_c), 
+		title(title_c), 
+		priority(priority_c), 
+		status(status_c), 
+		assignee(assignee_c) {};
+
 	
 };
 
