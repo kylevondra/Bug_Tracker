fb6355b161475d27b2087baa79b050bb98ffe9ae
Donovan <Dondo429@gmail.com>
2020-11-23 21:26:42 -0800

    Created 4 basic Bug Variables in main
    Testing during class, not a final update

--- Bug_Tracker/Source.cpp
+++ Bug_Tracker/Source.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 #include <string>
 
+use namespace std;
 
 int main() {
+	Bug a(123456, "Code Doesn't Compile", 1, 1, "bob@gmail.com");
+	Bug b(222222, "Syntax error occurs at line 15", 0, 2, "Donovan@yahoo.com");
+	Bug c(663636, "Code Doesn't Compile", 0, 0, "Kyle@hotmail.com");
+	Bug d(123123, "Code creates", 2, 2, "Matt@aol.com");
 
 	return 0;
 }
