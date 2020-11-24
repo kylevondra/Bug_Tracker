9e42470f3e763737164839e23e56bbfdace4f3a8
Kyle Vondra <k.vondra95@gmail.com>
2020-11-23 20:21:33 -0800

    added bug class attributes

--- Bug_Tracker/Bug.h
+++ Bug_Tracker/Bug.h
@@ -1,6 +1,23 @@
 #pragma once
+#include <string>
+#include <random>
+
+#define string std::string
+
 class Bug
 {
+private:
+	string ID;
+	string subject;
+	string priority;
+	string category;
+	string assignee;
+	string startDate;
+	string endDate;
+	string version;
 
+private:
+	
 };
+
 
